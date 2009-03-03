2d41dd8f6f550c7d875cd6cb1b8fb1b10799cda5
Douglas Bagnall <douglas@paradise.net.nz>
2009-03-03 23:34:57 +1300

    Beginning slow import, and rearrangement of file system recursion
    The slow import process might not work.

--- tools/rcs/core.py
+++ tools/rcs/core.py
@@ -1,4 +1,4 @@
-import sys, os
+import sys, os, subprocess
 
 
 class RCSError(Exception):
@@ -22,14 +22,32 @@ class Version:
         write(data)
         write('\n')
 
-    def to_git(self, write=sys.stdout.write):
-        write("commit %s\n" % self.gitref)
+    def to_git(self, branch=None, write=sys.stdout.write):
+        if branch is None:
+            branch = self.gitref
+        write("commit %s\n" % branch)
         write("committer %s <%s@flossmanuals.net> %s +0000\n" %
               (self.author, self.author, self.date))
         self._data_blob("TWiki import: %s revision %s" % (self.name, self.revision))
         write('M 644 inline %s\n' % (self.name))
         self._data_blob(self.contents)
         write("\n")
+
+    def to_git_slow(self, branch=None, write=sys.stdout.write):
+        """Write to git via the working tree"""
+        if branch is not None:
+            raise NotImplementedError("can't set branches in slow import, yet")
+        # put the new version there
+        f = open(self.name, 'w')
+        f.write(self.contents)
+        f.close()
+        subprocess.check_call(["git", "commit",
+                               "--author=%s@flossmanuals.net" % self.author,
+                               "-m", "TWiki import: %s rev. %s" % (self.name, self.revision),
+                               self.name])
+
+
+
 
     def set_date(self, date):
         raise NotImplementedError("subclass me, please")
