1bde89b10a61c14d219cb8c3b43c8ccf81de6941
Aleksandar Erkalovic <aerkalov@gmail.com>
2009-10-14 21:14:33 +0200

    Can accept /export and /export/ as valid URL now

--- lib/booki/editor/urls.py
+++ lib/booki/editor/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
     # utils                       
     url(r'^(?P<edition>[\w\s\_\.\-]+)/_utils/thumbnail/(?P<attachment>.*)$',  'booki.editor.views.thumbnail_attachment', name='thumbnail_attachment'),
 
-    url(r'^(?P<edition>[\w\s\_\.\-]+)/export/$',  'booki.editor.views.view_export', name='export_booki'),
+    url(r'^(?P<edition>[\w\s\_\.\-]+)/export/{0,1}$',  'booki.editor.views.view_export', name='export_booki'),
     url(r'^(?P<edition>[\w\s\_\.\-]+)/static/(?P<attachment>.*)$',  'booki.editor.views.view_attachment', name='view_attachment'),
 
 
