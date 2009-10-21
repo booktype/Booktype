700441d6f6f5528969e97eb000237f8dc385bc50
Aleksandar Erkalovic <aerkalov@gmail.com>
2009-10-21 21:42:42 +0200

    Add booki logo into templates.

--- lib/booki/account/views.py
+++ lib/booki/account/views.py
@@ -61,6 +61,9 @@ class ProjectForm(forms.Form):
 class ImportForm(forms.Form):
     archive_id = forms.CharField(required=False)
     
+class ImportFlossForm(forms.Form):
+    floss_book = forms.CharField(required=False)
+
 
 def view_profile(request, username):
     from django.contrib.auth.models import User
@@ -73,16 +76,20 @@ def view_profile(request, username):
     if request.method == 'POST':
         project_form = ProjectForm(request.POST)
         import_form = ImportForm(request.POST)
+#        floss_form = ImportFlossForm(request.POST)
 
         if import_form.is_valid() and import_form.cleaned_data["archive_id"] != "":
             from booki.editor import common
             common.importBookFromURL("http://objavi.flossmanuals.net/espri.cgi?mode=zip&book="+import_form.cleaned_data["archive_id"], createTOC = True)
 
+#        if floss_form.is_valid() and floss_form.cleaned_data["floss_book"] != "":
+#            from booki.editor import common
+#            common.importBookFromURL("http://objavi.flossmanuals.net/booki-twiki-gateway.cgi?server=en.flossmanuals.net&mode=zip&book="+floss_form.cleaned_data["floss_book"], createTOC = True)
 
         if project_form.is_valid() and project_form.cleaned_data["title"] != "":
             title = project_form.cleaned_data["title"]
             url_title = slugify(title)
- #           url_title = project_form.cleaned_data["url_title"]
+#           url_title = project_form.cleaned_data["url_title"]
             
 
             project = models.Project(url_name = url_title,
@@ -106,8 +113,9 @@ def view_profile(request, username):
     else:
         project_form = ProjectForm()
         import_form = ImportForm()
+        floss_form = ImportFlossForm()
 
     books = models.Book.objects.all()
 
-    return render_to_response('account/view_profile.html', {"request": request, "user": user, "project_form": project_form, "import_form": import_form, "books": books})
+    return render_to_response('account/view_profile.html', {"request": request, "user": user, "project_form": project_form, "import_form": import_form, "floss_form": floss_form, "books": books})
 
