21aae2891d6f794755ba9f0885cddd1f140a1119
Aleksandar Erkalovic <aerkalov@gmail.com>
2011-01-05 23:26:38 +0100

    Fix book import management script.

--- lib/booki/editor/management/commands/importbook.py
+++ lib/booki/editor/management/commands/importbook.py
@@ -1,16 +1,24 @@
 from django.core.management.base import BaseCommand, NoArgsCommand
+from optparse import make_option
 from booki.editor import common
 
 class Command(BaseCommand):
     help = "Imports book from book-zip."
+
+    option_list = BaseCommand.option_list + (
+        make_option('--user',
+            action='store',
+            dest='user',
+            default='booki',
+            help='Who is owner of the book'),)
 
     requires_model_validation = False
 
     def handle(self, *args, **options):
         from django.contrib.auth.models import User
 
-        user = User.objects.get(username="booki")
+        user = User.objects.get(username=options['user'])
 
         for fileName in args:
-            common.importBookFromFileTheOldWay(user, fileName)
+            common.importBookFromFile(user, fileName, createTOC=True)
         
