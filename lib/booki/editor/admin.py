d210cd93203d4354de3f58a49a1132e6ffc6db0a
Aleksandar Erkalovic <aerkalov@gmail.com>
2011-12-19 17:13:23 +0100

    #571. Add custom forms for User in Django Administrator

--- lib/booki/editor/admin.py
+++ lib/booki/editor/admin.py
@@ -1,6 +1,23 @@
 import models
 from django.contrib import admin
 
+from booki.account.models import UserProfile
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    fk_name = 'user'
+    max_num = 1
+
+class UserAdmin(AuthUserAdmin):
+    inlines = [UserProfileInline,]
+
+# unregister old user admin
+admin.site.unregister(User)
+# register new user admin
+admin.site.register(User, UserAdmin)
+
 admin.site.register(models.License)
 admin.site.register(models.Book)
 admin.site.register(models.Info)
