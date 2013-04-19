09b997d2814a3c401e7f6da4ba5a7250f0479da4
Daniel James <daniel@64studio.com>
2013-04-19 17:18:20 +0100

    Packaging updates for 1.6.0 release

--- gen-snapshot.sh
+++ gen-snapshot.sh
@@ -2,9 +2,8 @@
 # Script for generating nightly Booktype snapshot packages
 # Run from the directory containg the files checked out from git
 
-VERSION=1.5.4~$(date "+%Y%m%d")
+VERSION=1.6.0~$(date "+%Y%m%d")
 BUILDDEST=/tmp/booktype-${VERSION}/
-DEBDIR=`pwd`/debian
 
 git checkout devel
 git pull
@@ -12,35 +11,32 @@ git pull
 echo "cleaning up previous build..."
 
 rm -rf /tmp/booktype-*
-mkdir -p ${BUILDDEST}booktype
+mkdir ${BUILDDEST}
 
 echo "copying files to temporary directory..."
 
-cp -a * ${BUILDDEST}booktype || exit
-cp -a $DEBDIR ${BUILDDEST}debian || exit
+cp -a * ${BUILDDEST} || exit
 
 cd ${BUILDDEST} || exit
 
 # Set the version of the snapshot package
 
-sed -i "1s:(1.5.4-1):(${VERSION}):g" debian/changelog
+sed -i "1s:(1.6.0-1):(${VERSION}):g" debian/changelog
 
-# Fixes for 1.5.4  #############
+# Fixes for 1.6.0  #############
 
 # remove directories not needed by users
 rm -r tests
 rm -r tools
 
 # moved to debian/copyright
-rm AUTHORS.txt
+#rm AUTHORS.txt
 rm LICENSE.txt
 rm lib/booki/site_static/js/tiny_mce/classes/firebug/FIREBUG.LICENSE
 rm lib/booki/site_static/js/tiny_mce/license.txt
 rm lib/booki/site_static/js/jquery/AUTHORS.txt
 rm lib/booki/site_static/js/jquery/MIT-LICENSE.txt
 rm lib/booki/site_static/js/jquery/GPL-LICENSE.txt
-
-#Fix permissions
 
 #############################
 
