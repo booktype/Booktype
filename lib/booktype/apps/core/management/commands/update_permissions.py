46195d1084e4617df0d17f00a231c0b910cfa787
Helmy Giacoman <helmygb@gmail.com>
2014-10-24 21:34:01 -0600

    BK-1294 Delete permissions with update_permissions sync command

--- lib/booktype/apps/core/management/commands/update_permissions.py
+++ lib/booktype/apps/core/management/commands/update_permissions.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
-from django.core.management.base import BaseCommand
+from optparse import make_option
 from django.conf import settings
+from django.core.management.base import BaseCommand
 
+from booktype.apps.core.models import Permission
 from booktype.utils.permissions import (
     create_permissions, permissions_for_app
 )
 
+BOLD_ON = '\033[1m'
+BOLD_OFF = '\033[0m'
 
 class Command(BaseCommand):
     help = 'Reloads permissions for specified apps, or all apps if \
@@ -13,7 +17,31 @@ class Command(BaseCommand):
 
     # TODO: load permissions just for one app specified as param
 
+    option_list = BaseCommand.option_list + (
+        make_option(
+            '--delete-orphans', action='store_true',
+            help='Specify this param to delete undeclared permissions'
+        ),
+    )
+
     def handle(self, *args, **options):
+        saved_perms = []
+
         for app_name in settings.INSTALLED_APPS:
-            perms = permissions_for_app(app_name)
-            create_permissions(app_name, perms)
+            app_perms = permissions_for_app(app_name)
+            saved_perms += create_permissions(app_name, app_perms)
+
+        saved_perms_ids = [p.pk for p in saved_perms]
+        orphan_perms = Permission.objects.exclude(id__in=saved_perms_ids)
+
+        if options['delete_orphans']:
+            orphan_perms.delete()
+            print BOLD_ON + "All undeclared permissions has been deleted." + BOLD_OFF
+        else:
+            if orphan_perms.count() > 0:
+                suggestion = (
+                    "There are %s undeclared permissions. "
+                    "To delete them use: \n"
+                    "./manage.py update_permissions --delete-orphans"
+                )
+                print BOLD_ON + suggestion % orphan_perms.count() + BOLD_OFF
