ca1994af947866fae408d50bc6ab5e98b9c9a5a5
Helmy Giacoman <helmygb@gmail.com>
2014-10-29 20:43:13 -0600

    BK-1289 Book metadata in the editing interface

--- lib/booktype/apps/edit/forms.py
+++ lib/booktype/apps/edit/forms.py
@@ -2,10 +2,12 @@
 from django import forms
 from django.utils.translation import ugettext as _
 from booktype.apps.portal.forms import SpanErrorList
-from booki.editor.models import Language, Info, License, AttributionExclude, ChapterHistory
-from django.contrib.auth.models import User
+from booktype.apps.core.forms import BaseBooktypeForm
+from booki.editor.models import (
+    Language, Info, License)
 
-class BaseSettingsForm(object):
+
+class BaseSettingsForm(BaseBooktypeForm):
     success_url = None
     success_message = None
 
@@ -15,7 +17,7 @@ class BaseSettingsForm(object):
         super(BaseSettingsForm, self).__init__(*args, **kwargs)
 
     @classmethod
-    def initial_data(cls):
+    def initial_data(cls, book=None, request=None):
         return None
 
     @classmethod
@@ -25,16 +27,18 @@ class BaseSettingsForm(object):
     def save_settings(self, request):
         pass
 
+
 class LanguageForm(BaseSettingsForm, forms.Form):
     language = forms.ModelChoiceField(
-            label=_('Language'),
-            queryset=Language.objects.all()
-        )
+        label=_('Language'),
+        queryset=Language.objects.all()
+    )
     right_to_left = forms.BooleanField(
-            label=_('Right to left text'),
-            required=False,
-            help_text=_("Book with right to left writting.")
-        )
+        label=_('Right to left text'),
+        required=False,
+        help_text=_("Book with right to left writting.")
+    )
+    skip_select_and_checkbox = True
 
     @classmethod
     def initial_data(cls, book=None, request=None):
@@ -63,11 +67,13 @@ class LanguageForm(BaseSettingsForm, forms.Form):
             rtl = Info(book=book, kind=0, value_string=rtl_value)
             rtl.save()
 
+
 class LicenseForm(BaseSettingsForm, forms.Form):
     license = forms.ModelChoiceField(
-            label=_('License'),
-            queryset=License.objects.all().order_by("name")
-        )
+        label=_('License'),
+        queryset=License.objects.all().order_by("name")
+    )
+    skip_select_and_checkbox = True
 
     @classmethod
     def initial_data(cls, book=None, request=None):
@@ -79,5 +85,79 @@ class LicenseForm(BaseSettingsForm, forms.Form):
         book.license = self.cleaned_data['license']
         book.save()
 
+
 class ChapterStatus(BaseSettingsForm, forms.Form):
     pass
+
+
+# METADATA Standards
+DC = 'DC'  # Dublic Core
+BKM = 'BKM'  # Booktype Metadata
+
+# I'm using a list instead of a dict because I want
+# to mantain certain order on form fields
+METADATA_FIELDS = [
+    ('title', _('Title'), DC),
+    ('short_title', _('Short title'), BKM),
+    ('subtitle', _('Subtitle'), BKM),
+    ('short_description', _('Short description'), BKM),
+    ('long_description', _('Long description'), BKM),
+    ('publisher', _('Publisher'), DC),
+    ('publisher_city', _('Publisher city'), BKM),
+    ('publication_date', _('Publication date'), BKM),  # there is a date in DC
+    ('copyright_year', _('Copyright year'), BKM),
+    ('copyright_holder', _('Copyright holder'), BKM),
+    ('ebook_isbn', _('Ebook ISBN'), BKM),
+    ('print_isbn', _('Print ISBN'), BKM)
+]
+
+
+class MetadataForm(BaseSettingsForm, forms.Form):
+
+    def __init__(self, *args, **kwargs):
+        super(MetadataForm, self).__init__(*args, **kwargs)
+
+        for field, label, standard in METADATA_FIELDS:
+            field_name = '%s.%s' % (standard, field)
+            self.fields[field_name] = forms.CharField(
+                label=label, required=False)
+
+            c_field = self.fields[field_name]
+            BaseBooktypeForm.apply_class(c_field, 'form-control')
+
+            # apply widgets if needed
+            if field in self.Meta.widgets:
+                c_field.widget = self.Meta.widgets[field]
+
+    class Meta:
+        text_area = forms.Textarea(attrs={'class': 'form-control'})
+        widgets = {
+            'short_description': text_area,
+            'long_description': text_area
+        }
+
+    @classmethod
+    def initial_data(cls, book=None, request=None):
+        initial = {}
+        form_fields = ['%s.%s' % (f[2], f[0]) for f in METADATA_FIELDS]
+
+        for meta in Info.objects.filter(book=book):
+            if meta.name in form_fields:
+                initial[meta.name] = meta.value_string
+
+        return initial
+
+    def save_settings(self, book, request):
+        STRING = 0
+
+        for key, value in self.cleaned_data.items():
+            valid_value = self.cleaned_data.get(key, None)
+
+            if key in self.fields.keys() and valid_value:
+                meta, _ = Info.objects.get_or_create(
+                    book=book, name=key,
+                    kind=STRING
+                )
+
+                meta.value_string = value
+                meta.save()
