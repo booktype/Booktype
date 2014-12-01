dd6b6160335b9862208305c2da7a5901ebc6b62a
David Haberthür <email@davidhaberthuer.ch>
2014-12-01 14:17:59 +0100

    BK-1323 Reformat README and setup file
    `setup.py` is now fully `pep8` compliant
    
    As part of http://24pullrequests.com/

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 import os
 
+
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
@@ -21,37 +22,38 @@ long_description = (
 
 
 setup(
-    name = "Booktype",
-    version = "2.0.0",
+    name="Booktype",
+    version="2.0.0",
 
-    packages = find_packages('lib'),  # include all packages under lib
-    package_dir = {'':'lib'},   # tell distutils packages are under lib
+    packages=find_packages('lib'),  # include all packages under lib
+    package_dir={'': 'lib'},   # tell distutils packages are under lib
 
-    author = "Aleksandar Erkalovic",
-    author_email = "aerkalov@gmail.com",
+    author="Aleksandar Erkalovic",
+    author_email="aerkalov@gmail.com",
 
-    description = "Booktype is a free, open source platform that produces beautiful, engaging books formatted for print, Amazon, iBooks and almost any ereader within minutes.",
-    long_description = long_description,
+    description="Booktype is a free, open source platform that produces" + \
+        "beautiful, engaging books formatted for print, Amazon, iBooks and" + \
+        "almost any ereader within minutes.",
+    long_description=long_description,
 
-    url = "http://booktype.sourcefabric.org/",
-    include_package_data = True,
-    package_data = {
+    url="http://booktype.sourcefabric.org/",
+    include_package_data=True,
+    package_data={
         # If any package contains *.txt or *.rst files, include them
         '': ['*.txt', '*.rst'],
     },
     scripts=['scripts/createbooktype'],
 
-    install_requires = ['setuptools', 'simplejson', 'django' ],
+    install_requires=['setuptools', 'simplejson', 'django'],
     classifiers=[
           "License :: OSI Approved :: GNU General Public License (GPL)",
           "Programming Language :: Python",
           "Development Status :: 3 - Alpha",
           "Intended Audience :: Developers",
-	  "Intended Audience :: Information Technology",
-	  "Framework :: Django",
+      "Intended Audience :: Information Technology",
+      "Framework :: Django",
       ],
       keywords='collaborative book writing sprint',
       license='AGPL',
 
 )
-
