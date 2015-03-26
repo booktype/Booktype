1350420fab324506074869d32ff343129f6ba3be
Aleksandar Erkalovic <aerkalov@gmail.com>
2015-03-26 10:51:25 +0100

    BK-1448 Import fails for some input files

--- lib/booktype/apps/importer/views.py
+++ lib/booktype/apps/importer/views.py
@@ -3,6 +3,7 @@ import os
 import tempfile
 import datetime
 import importlib
+import logging
 
 from ebooklib import epub
 
@@ -31,6 +32,8 @@ from booktype.utils.misc import (
 from .forms import UploadForm, UploadBookForm
 
 
+logger = logging.getLogger("booktype.importer")
+
 IMPORTER_MAP = {
     '.epub': ('booktype.importer.epub', 'import_epub'),
     '.docx': ('booktype.importer.docx', 'import_docx')
@@ -45,7 +48,7 @@ class ImporterView(JSONResponseMixin, FormView):
         return form_class(data=request.POST, files=request.FILES)
 
     def file_extension(self, filename):
-        _, ext = os.path.splitext(os.path.basename(filename))
+        _, ext = os.path.splitext(os.path.basename(filename.lower()))
         return ext
 
     def get_default_title(self, temp_file, ext):
@@ -69,20 +72,27 @@ class ImporterView(JSONResponseMixin, FormView):
         return getattr(module, import_func)
 
     def form_valid(self, form):
+        logger.debug('ImporterView::form_valid')
+
         book_file = self.request.FILES['book_file']
         ext = self.file_extension(book_file.name)
+
+        logger.debug('ImporterView::Importing file extension is "{}".'.format(ext.encode('utf8')))
 
         temp_file = tempfile.NamedTemporaryFile(
             prefix='importing-', suffix='%s' % ext, delete=False)
         temp_file = open(temp_file.name, 'wb+')
+
+        logger.debug('ImporterView::Saving temporary file {}.'.format(temp_file.name.encode('utf8')))
 
         for chunk in book_file.chunks():
             temp_file.write(chunk)
         temp_file.close()
         temp_file = temp_file.name
 
-        default_book_title = self.get_default_title(temp_file, ext)
+        default_book_title = self.get_default_title(temp_file, ext)        
         book_title = form.cleaned_data.get('book_title', default_book_title)
+        logger.debug('ImporterView::book_title="{}"" default_book_title="{}".'.format(book_title.encode('utf8'), default_book_title.encode('utf8')))
 
         # in case book title in form is empty string
         if len(book_title) == 0:
@@ -92,16 +102,21 @@ class ImporterView(JSONResponseMixin, FormView):
             registered = Book.objects.filter(
                 title__startswith=book_title).count()
             book_title = '%s %s' % (book_title, registered)
+            logger.debug('ImporterView::Checking book availability: "{}".'.format(book_title.encode('utf8')))
 
         book_url = booktype_slugify(book_title)
         book = create_book(
             self.request.user, book_title, book_url=book_url)
+        logger.debug('ImporterView::Book created with url title="{}".'.format(book_url))
 
         # check if book will be hidden and set to book
         book_hidden = form.cleaned_data.get('hidden')
         if book_hidden:
             book.hidden = book_hidden
             book.save()
+            logger.debug('ImporterView::Setting book hidden.')
+        else:
+            logger.debug('ImporterView::Setting book visible.')
 
         notifier = CollectNotifier()
         delegate = Delegate()
@@ -110,6 +125,8 @@ class ImporterView(JSONResponseMixin, FormView):
         try:
             book_importer = self.get_importer(ext)
         except KeyError:
+            logger.error('ImporterView::No importer for this extension')
+
             response_data = {
                 'errors': [_('Extension not supported!')],
             }
@@ -121,8 +138,11 @@ class ImporterView(JSONResponseMixin, FormView):
                 notifier=notifier,
                 delegate=delegate
             )
+            logger.debug('ImporterView::Book imported.')
             response['url'] = reverse('reader:infopage', args=[book.url_title])
         except Exception as e:
+            logger.error('ImporterView::Some kind of error while importing book.')
+            logger.exception(e)
             notifier.errors.append(str(e))
 
         response['infos'] = notifier.infos
