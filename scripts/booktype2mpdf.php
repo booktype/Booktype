487b2c50bff272a1abbd943bbb947eda0f3f1e16
Aleksandar Erkalovic <aerkalov@gmail.com>
2015-07-08 18:04:46 +0200

    BK-1582 mPDF converter adds headers when they are needed

--- scripts/booktype2mpdf.php
+++ scripts/booktype2mpdf.php
@@ -29,14 +29,14 @@ if(file_exists($options["dir"]."/config.json")) {
 $html_data = file_get_contents($file_input);
 $html = substr($html_data, 12, strlen($html_data)-27);
 
-
 /* Create PDF */
+
 /*
 $mpdf = new mPDF('', array($config["config"]["page_width_bleed"], $config["config"]["page_height_bleed"]), '', '',
   $config["config"]["settings"]["gutter"], $config["config"]["settings"]["side_margin"],
   $config["config"]["settings"]["top_margin"], $config["config"]["settings"]["bottom_margin"],
-  $config["config"]["settings"]["header_margin"], $config["config"]["settings"]["footer_margin"]); */
-
+  $config["config"]["settings"]["header_margin"], $config["config"]["settings"]["footer_margin"]); 
+*/
 $mpdf = new mPDF();
 
 // Make it DOUBLE SIDED document
@@ -47,32 +47,43 @@ $mpdf->bleedMargin = $config["config"]["settings"]["bleed_size"];
 // Add first page and suppress page counter
 // When turned on, pages in ToC are not visible
 //$mpdf->AddPage('', '', '', '' , '1');
+$mpdf->AddPageByArray(array('suppress' => 'off'));
 
 $mpdf->h2toc = array(); 
 
 /* Add Styling */
 if(file_exists($options["dir"]."/style.css")) {
-    $data = file_get_contents($options["dir"]."/style.css");
-    $mpdf->WriteHTML($data, 1);
+    $css_data = file_get_contents($options["dir"]."/style.css");
+    $mpdf->WriteHTML($css_data, 1);
+}
+
+if(array_key_exists('show_footer', $config['config']['settings'])) {
+    if(isset($config['config']['settings']['show_footer'])) {
+      $html = "<sethtmlpagefooter name=\"footer-left\" page=\"E\" value=\"".
+               $config['config']['settings']['show_footer']."\" />
+               <sethtmlpagefooter name=\"footer-right\" page=\"O\" value=\"".
+               $config['config']['settings']['show_footer']."\" />  ".$html;
+    }
+}
+
+if(array_key_exists('show_header', $config['config']['settings'])) {
+    if(isset($config['config']['settings']['show_header'])) {
+      $html = "<sethtmlpageheader name=\"header-left\" page=\"E\" value=\"".
+               $config['config']['settings']['show_header']."\" show-this-page=\"0\" />
+               <sethtmlpageheader name=\"header-right\" page=\"O\" value=\"".
+               $config['config']['settings']['show_header']."\"  show-this-page=\"0\" />  ".$html;
+    }
 }
 
 
 /* Add Frontmatter */
 if(file_exists($options["dir"]."/frontmatter.html")) {
-   $data = file_get_contents($options["dir"]."/frontmatter.html");
-   $mpdf->WriteHTML($data, 2);
+   $frontmatter_data = file_get_contents($options["dir"]."/frontmatter.html");
+   $frontmatter_data = strtr($frontmatter_data, array('{TITLE}' => $config['metadata']['title']));
+
+   $mpdf->WriteHTML($frontmatter_data, 2);
 }
 
-
-if(array_key_exists('show_footer', $config['config']['settings'])) {
-    if(isset($config['config']['settings']['show_footer'])) {
-//       $mpdf->SetHTMLFooter('<div style="text-align: left;">{PAGENO}</div>', 'E');
-//       $mpdf->SetHTMLFooter('<div style="text-align: right;">{PAGENO}</div>', 'O');
-
-      $mpdf->WriteHTML("<sethtmlpagefooter name=\"footer-left\" page=\"E\" value=\"on\" />
-                        <sethtmlpagefooter name=\"footer-right\" page=\"O\" value=\"on\" />");
-    }
-}
 $mpdf->WriteHTML($html, 2);
 
 /* Add Endmatter */
