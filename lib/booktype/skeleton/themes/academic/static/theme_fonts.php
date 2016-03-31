6079280e708bdc8511c53e1ad3062c2caaeae6df
Daniel James <daniel@64studio.com>
2016-03-31 16:23:36 +0100

    BK-1385 Initial improvements to Academic theme

---
+++ lib/booktype/skeleton/themes/academic/static/theme_fonts.php
@@ -0,0 +1,12 @@
+/* Academic theme fonts */
+        "roboto" => array(
+                'R' => "Roboto-Regular.ttf",
+                'B' => "Roboto-Bold.ttf",
+                'I' => "Roboto-Italic.ttf",
+                'BI' => "Roboto-BoldItalic.ttf",
+        ),
+        "roboto-medium" => array(
+                'R' => "Roboto-Medium.ttf",
+                'I' => "Roboto-MediumItalic.ttf",
+        ),
+
