9dd461970bdffc11f2d63f30958e6a03ba7e56b7
MiczFlor <micz.flor@web.de>
2016-05-17 21:29:32 +0200

    added files for theme New Roman

---
+++ lib/booktype/skeleton/themes/newtimes/theme_fonts.php
@@ -0,0 +1,12 @@
+/* Academic theme fonts */
+        "times" => array(
+                'R' => "texgyretermes-regular.ttf",
+                'B' => "texgyretermes-bold.ttf",
+                'I' => "texgyretermes-italic.ttf",
+                'BI' => "texgyretermes-bolditalic.ttf",
+        ),
+        "times-medium" => array(
+                'R' => "texgyretermes-bold.ttf",
+                'I' => "texgyretermes-bolditalic.ttf",
+        ),
+
