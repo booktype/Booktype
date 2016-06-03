88cc4dc3d832a432453c828da9ba931f78102f0b
MiczFlor <micz.flor@web.de>
2016-06-03 15:27:10 +0200

    adding new themes

---
+++ lib/booktype/skeleton/themes/bauhaus/static/theme_fonts.php
@@ -0,0 +1,13 @@
+/* 'Bauhaus (Compact)' Version 0.1 theme fonts */
+        "texgyreadventor" => array(
+          'R' => "TeXGyreAdventor-Regular.ttf",
+          'B' => "TeXGyreAdventor-Bold.ttf",
+          'I' => "TeXGyreAdventor-Italic.ttf",
+          'BI' => "TeXGyreAdventor-BoldItalic.ttf",
+        ),
+        "texgyrecursor" => array(
+          'R' => "TeXGyreCursor-Regular.ttf",
+          'B' => "TeXGyreCursor-Bold.ttf",
+          'I' => "TeXGyreCursor-Italic.ttf",
+          'BI' => "TeXGyreCursor-BoldItalic.ttf",
+        ),
