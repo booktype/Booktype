5adb85c76b1cb68b6f50189130bbeb4d2d0795a0
Helmy Giacoman <helmygb@gmail.com>
2017-05-23 22:31:04 -0600

    BK-2272 ICML and DOCX converters should have a fallback path to scripts

--- lib/booktype/convert/pandoc/icml/converter.py
+++ lib/booktype/convert/pandoc/icml/converter.py
@@ -1,5 +1,6 @@
 import os
 import logging
+from unipath import Path
 
 from django.conf import settings
 
@@ -65,10 +66,14 @@ class ICMLConverter(BasePandocConverter):
         """
         temp_dir = os.path.join(os.path.dirname(epub_path), 'bash_temp')
 
+        BK_LIB_ROOT = Path(os.path.abspath(__file__)).ancestor(6)
+        default_path = '{}/scripts/epub2icml.sh'.format(BK_LIB_ROOT)
+        script_path = getattr(settings, 'PANDOC_ICML_SCRIPT', default_path)
+
         try:
             (_, out, err) = run_command(
                 'bash {} -i {} -o {} -p {} -t {}'.format(
-                    settings.PANDOC_ICML_SCRIPT,
+                    script_path,
                     epub_path,
                     zip_path,
                     settings.PANDOC_PATH,
