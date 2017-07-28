4824b69b7b4fb96bd22206843da2be8f92f549ad
olegpshenichniy <olegpshenichniy@gmail.com>
2017-07-28 11:29:00 +0200

    BK-2334 Use config.get_configuration for loading settings in context processor

--- lib/booktype/apps/core/context_processors.py
+++ lib/booktype/apps/core/context_processors.py
@@ -1,18 +1,22 @@
-from django.conf import settings as django_settings
+from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
+
+from booktype.utils import config
 
 
 # Inspired by https://github.com/mfogel/django-settings-context-processor
-def settings(request):
+def settings_variables(request):
     """
     Adds the settings specified in settings.TEMPLATE_VISIBLE_SETTINGS to
     the request context.
     """
     new_settings = {}
-    for attr in getattr(django_settings, "TEMPLATE_VISIBLE_SETTINGS", ()):
-        try:
-            new_settings[attr] = getattr(django_settings, attr)
-        except AttributeError:
+
+    for attr in getattr(settings, "TEMPLATE_VISIBLE_SETTINGS", ()):
+        new_settings[attr] = config.get_configuration(attr, 'NOVALUE')
+
+        if new_settings[attr] == 'NOVALUE':
             m = "TEMPLATE_VISIBLE_SETTINGS: '{0}' does not exist".format(attr)
             raise ImproperlyConfigured(m)
+
     return new_settings
