db162a5b2acbac4ac9ee169637f0751091e35136
olegpshenichniy <olegpshenichniy@gmail.com>
2018-04-17 14:04:23 +0200

    BK-2475 Fix broken tests

--- lib/booktype/apps/core/tests/test_templatetags.py
+++ lib/booktype/apps/core/tests/test_templatetags.py
@@ -10,22 +10,22 @@ class UsernameTest(TestCase):
         user = mock.Mock(username = 'Booktype')
         user.is_authenticated.return_value = False
 
-        self.assertEquals(booktype_tags.username(user), "Anonymous")
+        self.assertEquals(booktype_tags.tag_username(user), "Anonymous")
 
     def test_no_firstname(self):
         user = mock.Mock(username='booktype', first_name='')
         user.is_authenticated.return_value = True
 
-        self.assertEquals(booktype_tags.username(user), "booktype")
+        self.assertEquals(booktype_tags.tag_username(user), "booktype")
 
     def test_firstname(self):
         user = mock.Mock(username='booktype', first_name=' Booktype User ')
         user.is_authenticated.return_value = True
 
-        self.assertEquals(booktype_tags.username(user), ' Booktype User ')
+        self.assertEquals(booktype_tags.tag_username(user), ' Booktype User ')
 
     def test_firstname_empty(self):
         user = mock.Mock(username='booktype', first_name=' ')
         user.is_authenticated.return_value = True
 
-        self.assertEquals(booktype_tags.username(user), 'booktype')
+        self.assertEquals(booktype_tags.tag_username(user), 'booktype')
